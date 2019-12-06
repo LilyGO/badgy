1dde88c80ed6e30acfa43bbca311a51690b70259
LilyGO <31160602+LilyGO@users.noreply.github.com>
2019-12-06 18:15:08 +0800

    first commit

--- websocketDraw/websocketDraw.ino
+++ websocketDraw/websocketDraw.ino
@@ -1,32 +1,33 @@
 /* e-paper display lib */
 #include <GxEPD.h>
 //Use the GxGDEW029T5 class if you have Badgy Rev 2C. Make sure you are on GxEPD 3.05 or above
-#include <GxGDEW029T5/GxGDEW029T5.h>
-//Use the GxGDEH029A1 class if you have anything older
-//#include <GxGDEH029A1/GxGDEH029A1.h>
+//#include <GxGDEW029T5/GxGDEW029T5.h>
+#include <GxGDEH029A1/GxGDEH029A1.h>
 #include <GxIO/GxIO_SPI/GxIO_SPI.h>
 #include <GxIO/GxIO.h>
 /* include any other fonts you want to use https://github.com/adafruit/Adafruit-GFX-Library */
 #include <Fonts/FreeMonoBold9pt7b.h>
 /* WiFi  libs*/
-#include <ESP8266WiFi.h>
+#include <WiFi.h>
 #include <WiFiClient.h>
 #include <DNSServer.h>
-#include <ESP8266WebServer.h>
-#include <ESP8266HTTPUpdateServer.h>
-#include <WiFiManager.h>
+#include <WebServer.h>
+#include <HTTPUpdate.h>
+// #include <WiFiManager.h>
 #include <WebSocketsServer.h>
 #include <Hash.h>
+const char* ssid = "";
+const char* password = "";
 
 /* Always include the update server, or else you won't be able to do OTA updates! */
-/**/const int port = 8888;
-/**/ESP8266WebServer httpServer(port);
-/**/ESP8266HTTPUpdateServer httpUpdater;
+/**/const int port = 80;
+/**/WebServer httpServer(port);
+///**/ESP8266HTTPUpdateServer httpUpdater;
 /*                                                                                */
 
 /* Configure pins for display */
-GxIO_Class io(SPI, SS, 0, 2);
-GxEPD_Class display(io); // default selection of D4, D2
+GxIO_Class io(SPI, /*CS=5*/ SS, /*DC=*/ 17, /*RST=*/ 16); // arbitrary selection of 17, 16
+GxEPD_Class display(io, /*RST=*/ 16, /*BUSY=*/ 4);
 
 /* A single byte is used to store the button states for debouncing */
 byte buttonState = 0;
@@ -37,26 +38,47 @@ unsigned long debounceDelay = 50;    //the debounce time
 /* Initialize WebSocket on port 81*/
 WebSocketsServer webSocket = WebSocketsServer(81);
 
+void showText(char *text)
+{
+  display.setRotation(3); //even = portrait, odd = landscape
+  display.fillScreen(GxEPD_WHITE);
+  const GFXfont* f = &FreeMonoBold9pt7b ;
+  display.setTextColor(GxEPD_BLACK);
+  display.setFont(f);
+  display.setCursor(10,70);
+  display.println(text);
+  display.update();
+}
+
 void setup()
 {  
+  Serial.begin(115200);
   display.init();
   
-  pinMode(1,INPUT_PULLUP); //down
-  pinMode(3,INPUT_PULLUP); //left
-  pinMode(5,INPUT_PULLUP); //center
-  pinMode(12,INPUT_PULLUP); //right
-  pinMode(10,INPUT_PULLUP); //up
+  pinMode(37,INPUT_PULLUP); //down
+  pinMode(38,INPUT_PULLUP); //left
+  pinMode(39,INPUT_PULLUP); //center
 
-  /* WiFi Manager automatically connects using the saved credentials, if that fails it will go into AP mode */
-  WiFiManager wifiManager;
-  wifiManager.autoConnect("Badgy AP");
 
+   WiFi.begin(ssid, password);
   /* Once connected to WiFi, startup the OTA update server*/
-  httpUpdater.setup(&httpServer);
+//  httpUpdater.setup(&httpServer);
+    Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
   httpServer.begin();
   showIP();
-
-  /* Begin listening for WebSocket events*/
+//
+//  /* Begin listening for WebSocket events*/
   webSocket.begin();
   webSocket.onEvent(webSocketEvent);
 }
@@ -66,11 +88,9 @@ void loop()
   httpServer.handleClient();
   webSocket.loop();
   
-  byte reading =  (digitalRead(1)  == 0 ? 0 : (1<<0)) | //down
-                  (digitalRead(3)  == 0 ? 0 : (1<<1)) | //left
-                  (digitalRead(5)  == 0 ? 0 : (1<<2)) | //center
-                  (digitalRead(12) == 0 ? 0 : (1<<3)) | //right
-                  (digitalRead(10) == 0 ? 0 : (1<<4));  //up
+  byte reading =  (digitalRead(37)  == 0 ? 0 : (1<<0)) | //down
+                  (digitalRead(38)  == 0 ? 0 : (1<<1)) | //left
+                  (digitalRead(39)  == 0 ? 0 : (1<<2));  //center
                   
   if(reading != lastButtonState){
     lastDebounceTime = millis();
@@ -78,24 +98,20 @@ void loop()
   if((millis() - lastDebounceTime) > debounceDelay){
     if(reading != buttonState){
       buttonState = reading;
-      for(int i=0; i<5; i++){
+      for(int i=0; i<3; i++){
         if(bitRead(buttonState, i) == 0){
           switch(i){
             case 0:
               //do something when the user presses down
+              showText("You pressed the 2 button!");
               break;
             case 1:
               //do something when the user presses left
+              showText("You pressed the 1 button!");
               break;
             case 2:
               //do something when the user presses center
-              break;
-            case 3:
-              //do something when the user presses right
-              break;
-            case 4:
-              //do something when the user presses up
-              break;                              
+              showText("You pressed the 3 button!");              
             default:
               break;
           }
